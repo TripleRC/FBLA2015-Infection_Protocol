64a03feceb75fd5313643681e2232ab117ed6cea
Raphael Rouvinov-Kats <raphael.rouvinov@gmail.com>
2015-01-09 02:01:06 -0600

    Enemies work! Except they're invincible

--- Infection Protocol/Assets/Scripts/FirstPersonController.cs
+++ Infection Protocol/Assets/Scripts/FirstPersonController.cs
@@ -3,33 +3,33 @@ using System.Collections;
 
 public class FirstPersonController : MonoBehaviour {
 
-	public float movementSpeed = 8.0f;
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		//Rotation
-		float rotLeftRight = Input.GetAxis ("Mouse X");
-
-		transform.Rotate (0, rotLeftRight, 0);
-
-
-
-		//Movement
-		float fowardSpeed = Input.GetAxis ("Vertical") * movementSpeed;
-		float sideSpeed = Input.GetAxis ("Horizontal") * movementSpeed;
-
-		Vector3 speed = new Vector3( sideSpeed, 0, forwardSpeed);
-
-		speed = transform.rotation * speed;
-
-		CharacterController cc = GetComponent<CharacterController>();
-
-		cc.SimpleMove(speed);
-
-	}
+	//public float movementSpeed = 8.0f;
+	//
+	//// Use this for initialization
+	//void Start () {
+	//
+	//}
+	//
+	//// Update is called once per frame
+	//void Update () {
+	//	//Rotation
+	//	float rotLeftRight = Input.GetAxis ("Mouse X");
+	//
+	//	transform.Rotate (0, rotLeftRight, 0);
+	//
+	//
+	//
+	//	//Movement
+	//	float fowardSpeed = Input.GetAxis ("Vertical") * movementSpeed;
+	//	float sideSpeed = Input.GetAxis ("Horizontal") * movementSpeed;
+	//
+	//	Vector3 speed = new Vector3( sideSpeed, 0, forwardSpeed);
+	//
+	//	speed = transform.rotation * speed;
+	//
+	//	CharacterController cc = GetComponent<CharacterController>();
+	//
+	//	cc.SimpleMove(speed);
+	//
+	//}
 }
