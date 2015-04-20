a1e9dd6ad74c6411989179554d55d77ab2c46882
Raphael Rouvinov-Kats <raphael.rouvinov@gmail.com>
2015-04-20 01:37:40 -0500

    3rd person
    spent the whole day on this bs and it still stinks
    player turns to face where the mouse it pointed using a logistic
    equation, camera follows player
    camera also pans slightly
    
    I now understand how the one site made a day's worth of video on just
    how to make a 3rd person camera

--- Infection Protocol/Assets/Scripts/Camera/CameraFollow.cs
+++ Infection Protocol/Assets/Scripts/Camera/CameraFollow.cs
@@ -10,18 +10,51 @@ using System.Collections;
 public class CameraFollow : MonoBehaviour {
 
 	public Transform target;
-	public float smoothing = 5f;
+	public float smoothing = 6f;
 
 	Vector3 offset;
+	Vector3 offsetOrig;
 
 	void Start()
 	{
-		offset = transform.position - target.position;
+		if(offsetOrig == null) offsetOrig = target.transform.position - transform.position;
+		offset = target.transform.position - transform.position;
+
+		// old code for a camera that doesn't rotate
+		// offset = transform.position - target.position;
 	}
 
-	void FixedUpdate()
+	void LateUpdate()
 	{
-		Vector3 targetCamPos = target.position + offset;
-		transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing);
+		// turn the player based on the mouse's left/right movement
+		// float horizontal = Input.GetAxis ("Mouse X") * rotateSpeed;
+		// target.transform.Rotate (0, horizontal, 0);
+
+		// allow limit pitching of camera
+		offset.y = map (Input.mousePosition.y, 0, Screen.height, offsetOrig.y - 3f, offsetOrig.y + 0.5f);
+
+		// get angle of player and get a rotation from it
+		// float currentAngle = transform.eulerAngles.y;
+		float desiredAngle = target.transform.eulerAngles.y;
+		// float angle = Mathf.LerpAngle (currentAngle, desiredAngle, Time.deltaTime * smoothing);
+		Quaternion rotation = Quaternion.Euler (0, desiredAngle, 0);
+
+		// set your position to the player's position with your offset, 
+		// your offset including the rotation that was just calculated
+		transform.position = target.transform.position - (rotation * offset);
+
+		// make the camera face the player
+		transform.LookAt (target.transform);
+
+		// old code for a camera that doesn't rotate
+		// Vector3 targetCamPos = target.position + offset;
+		// transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing);
+	}
+
+	// maps input from input range to output range
+	// e.g. map(0.5, 0, 1, 10, 20) => 15
+	float map(float input, float inMin, float inMax, float outMin, float outMax)
+	{
+		return outMin + (outMax - outMin) * ((input - inMin)/(inMax - inMin));
 	}
 }
--- Infection Protocol/Assets/Scripts/Player/PlayerMovement.cs
+++ Infection Protocol/Assets/Scripts/Player/PlayerMovement.cs
@@ -7,13 +7,20 @@
  * */
 public class PlayerMovement : MonoBehaviour
 {
-	public float speed = 6f;
+	public float movementSpeed = 6f;
 
 	Vector3 movement;
 	Animator anim;
 	Rigidbody playerRigidBody;
 	int floorMask;
 	float camRayLength = 100f;
+
+	float maxTurnSpeed = 2.08f; // 3 ^ (1/1.5)
+	float minTurnSpeed = 0.01f;
+	float turnSpeedPower = 1.5f; // turnSpeeds are raised to this power;
+
+	float lastMouseX = 0;
+	// Vector3 screenCenter = new Vector3(Screen.width/2, Screen.height/2 + 7, 0); // +7 to look forward
 
 	void Awake()
 	{
@@ -24,25 +31,58 @@ public class PlayerMovement : MonoBehaviour
 
 	void FixedUpdate()
 	{
+
 		float h = Input.GetAxisRaw ("Horizontal");
 		float v = Input.GetAxisRaw ("Vertical");
 
 		Move (h, v);
 		Turning ();
 		Animating (h, v);
+
+		lastMouseX = Input.mousePosition.x;
+
+		/*
+		if (Input.GetKey(KeyCode.Escape))
+			Screen.lockCursor = false;
+		else
+			Screen.lockCursor = true;*/
 	}
 
 	void Move(float h, float v)
 	{
 		movement.Set (h, 0f, v);
 
-		movement = movement.normalized * speed * Time.deltaTime;
+		movement = movement.normalized * movementSpeed * Time.deltaTime;
 
-		playerRigidBody.MovePosition (transform.position + movement);
+		transform.Translate (movement);
+		// playerRigidBody.MovePosition (transform.position + movement);
 	}
 
 	void Turning()
 	{
+		/*Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast (ray, hit)) {
+			Vector3 lookTarget = hit.point;
+
+			// rotate towards target
+			Vector3 lookDelta = (hit.point-transform.position);
+			Quaternion targetRot = Quaternion.LookRotation(lookDelta);
+			float rotSpeed = turnSpeed * Time.deltaTime;
+			transform.rotation = Quaternion.RotateTowards( transform.rotation, targetRot, rotSpeed );
+		}*/
+
+		/*float currentAngle = transform.eulerAngles.y;
+		float desiredAngle = target.transform.eulerAngles.y;
+		float angle = Mathf.LerpAngle (currentAngle, desiredAngle, Time.deltaTime * smoothing);
+		Quaternion rotation = Quaternion.Euler (0, desiredAngle, 0);
+
+		transform.position = target.transform.position - (rotation * offset);
+
+		Vector3 newrotation = new Vector3 (0, Input.GetAxis ("Mouse X") - lastMouseX, 0);
+		transform.Rotate (0, transform.rotation + newrotation, 0);*/
+
+
 		Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
 
 		RaycastHit floorHit;
@@ -53,13 +93,72 @@ public class PlayerMovement : MonoBehaviour
 			playerToMouse.y = 0f;
 
 			Quaternion newRotation = Quaternion.LookRotation (playerToMouse);
-			playerRigidBody.MoveRotation (newRotation);
+			//newRotation.y /= 1f;
+
+			//playerRigidBody.MoveRotation (newRotation);
+
+			/*
+			float screenCenterX = Screen.width/2f;
+			float mouseX = Input.mousePosition.x;
+			float minDiffFromCenter = 0.5f;
+
+			// if your mouse is >1% from the center of the screen towards the edge
+			// set a rotation speed
+			if(Mathf.Abs(mouseX - screenCenterX)/(screenCenterX) > minDiffFromCenter)
+			{
+				float rotSpeed = 0;
+				if(mouseX < screenCenterX) // turning left
+				{
+					float screenLeftMin = 0;
+					float screenLeftMax = screenCenterX * (1 - minDiffFromCenter);
+					rotSpeed = -map(mouseX, screenLeftMin, screenLeftMax, -maxTurnSpeed, 0);
+
+				} else // turning right
+				{
+					float screenRightMin = screenCenterX * (1 + minDiffFromCenter);
+					float screenRightMax = Screen.width;
+					rotSpeed = map(mouseX, screenRightMin, screenRightMax, 0, maxTurnSpeed);
+				}
+				transform.rotation = Quaternion.RotateTowards(transform.rotation, newRotation, rotSpeed);
+			}*/
+
+			float rotSpeed = getRotationSpeed();
+			transform.rotation = Quaternion.RotateTowards(transform.rotation, newRotation, rotSpeed);
+
 		}
+
 	}
 
 	void Animating(float h, float v)
 	{
 		bool walking = h != 0f || v != 0f;
 		anim.SetBool ("IsWalking", walking);
+	}
+
+	// maps input from input range to output range
+	// e.g. map(0.5, 0, 1, 10, 20) => 15
+	float map(float input, float inMin, float inMax, float outMin, float outMax)
+	{
+		return outMin + (outMax - outMin) * ((input - inMin)/(inMax - inMin));
+	}
+
+	// returns appropriate rotation speed
+	// uses a logistic growth equation with carrying capacity of maxTurnSpeed, raised to a power
+	// k value of -8, and 'A' = maxTurnSpeed/minTurnSpeed
+	// P        =  M            / (1 + A         				 * e^(-kt)
+	// rotSpeed = (maxTurnSpeed / (1 + maxTurnSpeed/minTurnSpeed * e^(-8x)) ^ turnSpeedPower
+	// doesn't have to handle left/right negatives because Quaternion.RotateTwords does that
+	float getRotationSpeed()
+	{
+		float rotSpeed = 0;
+		float kVal = 20f;
+
+		float screenCenterX = Screen.width/2f;
+		float mouseX = Input.mousePosition.x;
+		float x = Mathf.Abs (mouseX - screenCenterX) / screenCenterX; // between 0 and 1
+		rotSpeed = maxTurnSpeed / (1 + maxTurnSpeed/minTurnSpeed * Mathf.Exp(-kVal * x));
+		rotSpeed = Mathf.Pow(rotSpeed, turnSpeedPower);
+
+		return rotSpeed;
 	}
 }
